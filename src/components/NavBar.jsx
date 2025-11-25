a494cd5a70f4eeab015278002d075ce9bb733926
hashantr99 <123436883+hashantr99@users.noreply.github.com>
2025-11-25 23:01:21 +0530

    nav bar part2

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react"
 
+const navLinks = ["About", "Spaces", "Location", "Contact"]
+
 const NavBar = () => {
 
     const [isScrolled, setIsScrolled] = useState(false)
@@ -17,7 +19,23 @@ const NavBar = () => {
     }, [])
 
   return (
-    <header>NavBar</header>
+    <header className={`fixed top-5 left-1/2 -translate-x-2 z-50 translate-all duration-300 ease-in rounded-full text-zinc-800 shadow-lg ${isScrolled ? "h-14 bg-zinc-200/40 backdrop-blur-xl border border-white/10 scale-95 w-[85%] max-w-4xl" : "h-14 bg-zinc-100 w-[90%] max-w-5xl"} px-6 flex items-center justify-between`}>
+      {/* Left Nav links */}
+      <div className="hidden md:flex gap-8 font-medium text-sm">
+        {navLinks.map((item) => (
+          <a  
+          key={item}
+          href={`#${item.toLowerCase()}`}
+          className="hover:-translate-y-0.5 hover:uppercase hover:text-rose-600 hover:font-bold transition-all duration-200 ease-in-out">
+            {item}
+          </a>
+        ))}
+      </div>
+
+{/* Logo */}
+<div className="text-2xl"></div>
+
+    </header>
   )
 }
 
